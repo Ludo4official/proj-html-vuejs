e7dad58e296cd13ca12c0c6235a4024b98588036
Ludovico Longhini <ludovico.longhini@gmail.com>
2023-02-15 10:29:41 +0100

    Progressi Progetto

--- src/AppHeader.vue
+++ src/AppHeader.vue
@@ -4,6 +4,18 @@ export default {
     name: 'AppHeader',
     components: {
         AppJumbo,
+    },
+    data() {
+        return {
+            links: [
+                'Home',
+                'Pages',
+                'Courses',
+                'Features',
+                'Blog',
+                'Shop'
+            ]
+        }
     }
 }
 </script>
@@ -14,18 +26,25 @@ export default {
     <div class="my-cnt">
         <div class="links">
             <ul>
-                <li>
-                    <a href="#">LINK</a>
+                <li v-for="link in links">
+                    <a href="#">{{ link }}</a>
                 </li>
             </ul>
         </div>
 
         <div class="logo">
-            LOGO
+            <img src="./assets/dark-logo.png" alt="MaxCoach">
         </div>
 
         <div class="user">
-            USER
+            <div class="icons" >
+                <i class="fa-solid fa-cart-shopping"></i>
+                <i class="fa-solid fa-user"></i>
+            </div>
+            <div class="search-bar">
+                <input type="text" name="" id="" placeholder="Search...">
+                <button><i class="fa-solid fa-magnifying-glass"></i></button>
+            </div>
         </div>
     </div>
 </div>
@@ -40,7 +59,6 @@ export default {
 .header {
     width: 100vw;
     height: 70px;
-    border-style: dashed;
     display: flex;
     justify-content: center;
 
@@ -48,31 +66,55 @@ export default {
     .my-cnt {
         width: 90%;
         height: 100%;
-        border-style: dashed;
         display: flex;
 
         .links, .user {
             width: 40%;
             height: 100%;
-            border-style: dashed;
             display: flex;
             align-items: center;
             justify-content: center;
 
             ul {
                 list-style: none;
+                display: flex;
 
                 li a {
                     text-decoration: none;
-                    color: black;
+                    color: #3f3a64;
+                    padding: 0px 15px;
+                    font-weight: bold;
                 }
+            }
+
+            .icons {
+                font-size: 1.25rem;
+                color: #3f3a64;
+
+                i {
+                    padding: 0px 15px;
+                    cursor: pointer;
+                }
+            }
+
+            .search-bar {
+
+                input, button {
+                    background-color: #f5f1ed;
+                    border-style: none;
+                    padding: 10px 20px;
+
+                    i {
+                        color: #20ad96;
+                    }
+                }
+                
             }
         }
 
         .logo {
             width: 20%;
             height: 100%;
-            border-style: dashed;
             display: flex;
             align-items: center;
             justify-content: center;
