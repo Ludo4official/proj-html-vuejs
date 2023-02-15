da40a5ba91c2c712cb24b6685fd6d47266f2bfad
Ludovico Longhini <ludovico.longhini@gmail.com>
2023-02-15 12:42:21 +0100

    Progressi progetto

--- src/AppFooter.vue
+++ src/AppFooter.vue
@@ -1,6 +1,25 @@
 <script>
 export default {
-    name: 'AppFooter'
+    name: 'AppFooter',
+
+    data() {
+        return {
+            explores: [
+                'Start Here',
+                'Blog',
+                'About us',
+                'Success story',
+                'Courses',
+                'Contact us'
+            ],
+            infos: [
+                'Membership',
+                'Purchase guide',
+                'Privacy policy',
+                'Terms of service'
+            ]
+        }
+    }
 }
 </script>
 
@@ -8,72 +27,59 @@ export default {
     
     <div class="footer">
         <div class="container">
-            <div class="row pt-5 fw-bold">
+            <div class="row pt-5">
                 <div class="col-6">
-                    <div>
-                        Adress
+                    <div class="fw-bold">
+                        Address
                     </div>
                     <ul>
                         <li>
-
+                            382 NE 191st St # 87394 Miami, FL 33179- 3899
                         </li>
                         <li>
-
+                            +1 (305) 547-9909 (9am - 5pm EST, Monday - Friday)
                         </li>
                         <li>
-
+                            support@maxcoach.com
                         </li>
                     </ul>
-                    <div>
-                        ICONS
+                    <div class="icons">
+                        <i class="fa-brands fa-square-facebook"></i>
+                        <i class="fa-brands fa-twitter"></i>
+                        <i class="fa-brands fa-instagram"></i>
+                        <i class="fa-brands fa-linkedin-in"></i>
                     </div>
                 </div>
                 <div class="col-3">
-                    <div>
+                    <div class="fw-bold">
                         Explore
                     </div>
                     <ul>
-                        <li>
-
-                        </li>
-                        <li>
-
-                        </li>
-                        <li>
-
-                        </li>
-                        <li>
-
-                        </li>
-                        <li>
-
+                        <li v-for="explore in explores">
+                            {{ explore }}
                         </li>
                     </ul>
                 </div>
                 <div class="col-3">
-                    <div>
+                    <div class="fw-bold">
                         Information
                     </div>
                     <ul>
-                        <li>
-
-                        </li>
-                        <li>
-
-                        </li>
-                        <li>
-
-                        </li>
-                        <li>
-
+                        <li v-for="info in infos">
+                            {{ info }}
                         </li>
                     </ul>
                 </div>
             </div>
             <div class="row text-center mt-5">
                 <div class="col">
-                    TESTO
+                    <i class="fa-regular fa-copyright"></i> 2020 Maxcoach. All Rights Reserved
                 </div>
+            </div>
+            <div>
+                <a href="#">
+                    <i class="fa-solid fa-arrow-up-long"></i>
+                </a>
             </div>
         </div>
     </div>
@@ -87,6 +93,45 @@ export default {
     min-height: 300px;
     margin-top: 200px;
     color: wheat;
+    position: relative;
+
+    ul {
+        list-style: none;
+        padding: 20px 0px;
+        
+        li {
+            text-decoration: none;
+            font-size: 0.8rem;
+        }
+    }
+
+    a {
+        position: absolute;
+        bottom: 5%;
+        right: 10%;
+        background-color: #20ad96;
+        color: white;
+        border-style: none;
+        border-radius: 50%;
+        line-height: 50px;
+        height: 50px;
+        width: 50px;
+        text-align: center ;
+    }
+
+    .icons {
+        color: #f5f1ed;
+        font-size: 1.2rem;
+
+        i {
+            padding: 0px 10px;
+
+            &:hover {
+                cursor: pointer;
+                color: #20ad96;
+            }
+        }
+    }
 }
 
 </style>
